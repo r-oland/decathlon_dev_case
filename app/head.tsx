247ea4b6b26c0fdb391bb033d1a8c02c996fa616
Roland Branten <roland.branten@gmail.com>
2023-01-19 15:45:26 +0100

    Init repo

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <title>Decathlon Dev Case</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="A dev case to showcase my skills ✨" />
+      {/* Import required fonts */}
+      <link rel="icon" href="/favicon.svg" />
+    </>
+  );
+}
---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Enable app directory
+  experimental: {
+    appDir: true,
+  },
+};
+
+module.exports = nextConfig;
